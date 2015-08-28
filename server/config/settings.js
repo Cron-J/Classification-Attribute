9fd9547880a8dbb6ae603db8d68cd3a131b94f4b
karanpatyal <ptlkaran499@gmail.com>
2015-08-28 10:39:22 +0530

    chnages for option value

--- server/config/settings.js
+++ server/config/settings.js
@@ -4,7 +4,7 @@
 var path = require('path')
 module.exports = {
     server: {
-        listenHost: '0.0.0.0',
+        listenHost: '127.0.0.1',
         listenPort: 8080,                                   // The port on which the server is to listen (means that the app is at http://localhost:8081 for instance)
         securePort: 8082,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8082 for instance)
         distFolder: path.resolve(__dirname, '../../client/dist'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
@@ -18,7 +18,7 @@ module.exports = {
         databaseName : "classification-attribute",
         userName : "event",
         password : "event",
-        url: "mongodb://gaurav:cronj123@novus.modulusmongo.net:27017/i4wOnupy"
+        url: "localhost:27017/classification-attribute"
 
     },
     configuration : {
